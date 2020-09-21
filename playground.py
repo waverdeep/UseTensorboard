1b77c6097ac340e06665f01e8a6cea63b9830a21
waverDeep <dandyleader@naver.com>
2020-09-21 19:09:10 +0900

    학습 코드 작성

--- playground.py
+++ playground.py
@@ -4,11 +4,46 @@ import dataloader
 import torchvision
 import model
 import torch
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+import numpy as np
 # 텐서보드를 사용해서 Projector를 구현할 때 오류가 있음
 # 이 오류를 해결하기 위해서 작성해야 할 것
 import tensorflow as tf
 import tensorboard as tb
 tf.io.gfile = tb.compat.tensorflow_stub.io.gfile
+
+
+def images_to_probs(net, images):
+    '''
+    학습된 신경망과 이미지 목록으로부터 예측 결과 및 확률을 생성합니다
+    '''
+    output = net(images)
+    # convert output probabilities to predicted class
+    _, preds_tensor = torch.max(output, 1)
+    preds = np.squeeze(preds_tensor.numpy())
+    return preds, [F.softmax(el, dim=0)[i].item() for i, el in zip(preds, output)]
+
+
+def plot_classes_preds(net, images, labels):
+    '''
+    학습된 신경망과 배치로부터 가져온 이미지 / 라벨을 사용하여 matplotlib
+    Figure를 생성합니다. 이는 신경망의 예측 결과 / 확률과 함께 정답을 보여주며,
+    예측 결과가 맞았는지 여부에 따라 색을 다르게 표시합니다. "images_to_probs"
+    함수를 사용합니다.
+    '''
+    preds, probs = images_to_probs(net, images)
+    # 배치에서 이미지를 가져와 예측 결과 / 정답과 함께 표시(plot)합니다
+    fig = plt.figure(figsize=(12, 48))
+    for idx in np.arange(4):
+        ax = fig.add_subplot(1, 4, idx+1, xticks=[], yticks=[])
+        dataloader.matplotlib_imshow(images[idx], one_channel=True)
+        ax.set_title("{0}, {1:.1f}%\n(label: {2})".format(
+            dataloader.classes[preds[idx]],
+            probs[idx] * 100.0,
+            dataloader.classes[labels[idx]]),
+                    color=("green" if preds[idx]==labels[idx].item() else "red"))
+    return fig
 
 
 # projector 관련 코드
@@ -60,6 +95,39 @@ def set_tensorboard(trainset, testset, trainloader, testloader, net):
                          label_img=images.unsqueeze(1))
     writer.close()
 
+
+def train(trainloader, testloader, net, optimizer, criterion, writer):
+    for epoch in range(1):  # 데이터셋을 여러번 반복
+
+        for i, data in enumerate(trainloader, 0):
+
+            # [inputs, labels]의 목록인 data로부터 입력을 받은 후;
+            inputs, labels = data
+
+            # 변화도(Gradient) 매개변수를 0으로 만들고
+            optimizer.zero_grad()
+
+            # 순전파 + 역전파 + 최적화를 한 후
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+            if i % 1000 == 999:  # 매 1000 미니배치마다...
+
+                # ...학습 중 손실(running loss)을 기록하고
+                writer.add_scalar('training loss',
+                                  running_loss / 1000,
+                                  epoch * len(trainloader) + i)
+
+                # ...무작위 미니배치(mini-batch)에 대한 모델의 예측 결과를 보여주도록
+                # Matplotlib Figure를 기록합니다
+                writer.add_figure('predictions vs. actuals',
+                                  plot_classes_preds(net, inputs, labels),
+                                  global_step=epoch * len(trainloader) + i)
+                running_loss = 0.0
+    print('Finished Training')
 
 def main():
     trainset, testset = dataloader.get_datasets()
